d7ea11d95dff611cfaa5a949c67a825a2476dd94
Delegates Account <delegates@lums.edu.pk>
2025-06-24 10:40:54 +0500

    initial commit
    Just copied the files, nothing else

---
+++ script.js
@@ -0,0 +1,7 @@
+function changeIntro() {
+    const intro = document.getElementById('intro');
+    intro.textContent = "Thanks for visiting my page! I'm currently open to exciting new opportunities.";
+  }
+  
+
+  
